0ddbaca9845f66fc95471e92bd7e85ae54d501cc
Abdou Ghariani <aghariani@pivotal.io>
2017-10-18 17:58:36 +0200

    Various improvements
    Signed-off-by: Joseph Taylor <jtaylor@pivotal.io>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-/* global YT */
+/* global YT,  */
 
 class YouTube extends Component {
 
@@ -16,11 +16,14 @@ class YouTube extends Component {
       const player = new YT.Player('player', {
         height: '160',
         width: '160',
-        playerVars: {'controls': 0}
+        playerVars: {
+          'controls': 0, 
+          'playsinline': 1,
+        },
       });
 
       this.setState({player: player})
-    }
+    };
 
     const tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
@@ -46,7 +49,7 @@ class YouTube extends Component {
   render() {
     return(
       <div id="player-wrapper">
-        <div id="player"></div>
+        <div id="player" />
       </div>
     )
   }
@@ -83,13 +86,21 @@ class Timer extends Component {
       return;
     }
 
+    if (this.state.currentCount <= 0) {
+      this.props.timesUp();
+      this.setState({ 
+        currentCount: this.props.startingTime * 60 * 1000
+      });
+      return;
+    }
+
     this.setState({ 
       currentCount: this.state.currentCount - 1000 
     });
   }
 
   render() {
-      return (<section>{
+      return (<section className="remaining">{
         new Date(this.state.currentCount)
           .toISOString()
           .substr(14, 5)
@@ -110,52 +121,110 @@ class App extends Component {
     this.library = [
       {
         length: 5,
-        tracks: ['GcHL8efKKPE']
+        tracks: [{
+          id: 'GcHL8efKKPE',
+          name: 'The Clash - The Magnificent Seven'
+        }, {
+          id: 'WQYsGWh_vpE',
+          name: 'Portishead - Roads'
+        }, {
+          id: '5OHBEmQtNIg',
+          name: 'Nirvana - Where Did You Sleep Last Night'
+        }]
       },
       {
         length: 6,
-        tracks: ['W6FZwVvS8_8']
+        tracks: [{
+          id: 'W6FZwVvS8_8',
+          name: 'The Clash - Police & Thieves'
+        }, {
+          id: 'so-L12LFRR8',
+          name: 'New Order - Your Silent Face'
+        }, {
+          id: 'evqx5HLm9iM',
+          name: 'Daft Punk - One More Time / Aerodynamic'
+        }, {
+          id: 'O4o8TeqKhgY',
+          name: 'Grandmaster Flash – The Message'
+        }]
       },
       {
-        length: 7,
-        tracks: []
-      },
-      {
-        length: 8,
-        tracks: []
+        length: 10,
+        tracks: [{
+          id: '73_UmjJBi6E',
+          name: 'The Doors - The End'
+        }, {
+          id: 'WZbE2VvZCqk',
+          name: 'Idris Muhammad - Loran\'s Dance'
+        },]
       }
     ];
   }
 
   handleClick(time) {
-
     let isRunning = this.state.time !== time || !this.state.isRunning;
-    let track = this.library.find(i => i.length === time).tracks[0];
+    
+    let track;
+    if (isRunning) {
+      let tracks = this.library.find(i => i.length === time).tracks;
+      track = tracks[Math.floor(Math.random() * tracks.length)]; 
+    } else {
+      track = { 
+        id: this.state.trackToPlay,
+        name: this.state.trackName
+      };
+    }
 
     this.setState({
       time: time,
       isRunning: isRunning,
-      trackToPlay: track,
+      trackToPlay: track.id,
+      trackName: track.name,
+    });
+  }
+
+  timesUp() {
+    this.setState({
+      isRunning: false
     });
   }
 
   renderButtons() {
-    return this.library.map(i => i.length).map(i =>
-        <input type='button' key={i}
-          value={ i + "min" }
-          className={(this.state.time === i ? 'active' : '')}
+    return this.library.map(i => i.length).map(i => {
+      const className = this.state.time === i ? 'active' : '';
+      const action = this.state.time === i && this.state.isRunning ? 'fa-pause' : 'fa-play';
+      return (
+        <button key={i}
+          className={className}
           onClick={() => this.handleClick(i)}
-        />);
+        >
+        { `${i}min ` }<i className={'fa ' + action}/>
+        </button>
+      )
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <div className="time">
-          <Timer startingTime={this.state.time} isRunning={this.state.isRunning} />
+        <div className="title">
+          <img alt="logo" src="logo.svg" />
+          <h1>Darbouka</h1>
+          <p>Musical timer for creative sessions</p>
+        </div>
+        <div className="status-panel">
+          <div className="vertical-centre">
+            <Timer startingTime={this.state.time} 
+              isRunning={this.state.isRunning}
+              timesUp={() => this.timesUp()}
+            />
+            <section className="trackName">{this.state.trackName}</section>
+          </div>
         </div>
         <div className='buttons'>{ this.renderButtons() }</div>
-        <YouTube track={this.state.trackToPlay} playing={this.state.isRunning}/>
+        <YouTube track={this.state.trackToPlay} 
+          playing={this.state.isRunning}
+        />
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, render } from 'enzyme';
+import * as Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const wrapper = render(<App />);
+  const timer = wrapper.find('section');
+  expect(timer.length).toBeGreaterThan(0)
+});
+
+
+describe('<App />', () => {
+  describe('renderButtons', () => {
+    it('should render four buttons', () => {
+      const app = new App();
+      const buttons = app.renderButtons();
+      expect(buttons.length).toBe(app.library.length);
+      expect(buttons.map(b => parseInt(b.props.children[0])))
+        .toEqual([5,6,10]);
+    });
+  });
+
+  describe('handleClick', () => {
+    describe('when it is already running', () => {
+      const app = shallow(<App />);
+      app.handleClick(5);
+    });
+  });
 });
